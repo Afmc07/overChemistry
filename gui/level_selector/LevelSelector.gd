5e919e338e9a36fe12e8a6fa7d9e02e6d088eab5
Emanuel <emanuel10.01@gmail.com>
2022-10-12 17:37:08 -0300

    Return button and exit confirmation

--- gui/level_selector/LevelSelector.gd
+++ gui/level_selector/LevelSelector.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_Level1Button_pressed():
 	get_parent().change_scene("res://maps/tutorials/TutorialCarbon.tscn")
+
+
+func _on_BackButton_pressed():
+	get_parent().change_scene("res://gui/menu/Menu.tscn")
