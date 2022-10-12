ef380001f9dd0cff958ce548d446954478cf1c53
Artur Souto <artursm007.2@gmail.com>
2022-10-12 14:46:49 -0300

    ajustes ao vivo

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ onready var current_scene = $Menu
 
 
 func select_level() -> void:
-	var next_scene = load("res://Node2D.tscn").instance()
+	var next_scene = load("res://maps/tutorials/TutorialCarbon.tscn").instance()
 	current_scene.queue_free()
 	add_child(next_scene)
 	current_scene = next_scene
