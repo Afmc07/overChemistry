8b314af47358d2aadda47e7f392f8097da5adc0f
Parjasnelly A <63004038+parjasnelly@users.noreply.github.com>
2022-12-01 10:14:44 -0300

    Ferreiro e melhorias no menu

--- gui/level_selector/LevelSelector.gd
+++ gui/level_selector/LevelSelector.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 func _on_Level1Button_pressed():
+	var node = get_tree().get_root().get_node("Main/AudioStreamPlayer")
+	node.stop()
 	get_parent().change_scene("res://maps/tutorials/TutorialCarbon.tscn")
 
 
