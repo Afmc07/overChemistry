cd2ccdb634f2ad3eb7ff6a58f81a81846ee873ca
Andres <afmc07@hotmail.com>
2022-12-01 17:30:56 -0300

    Level 2 Additions

--- gui/level_selector/LevelSelector.gd
+++ gui/level_selector/LevelSelector.gd
@@ -7,5 +7,10 @@ func _on_Level1Button_pressed():
 	get_parent().change_scene("res://maps/tutorials/TutorialCarbon.tscn")
 
 
+func _on_Level2Button_pressed():
+	var node = get_tree().get_root().get_node("Main/AudioStreamPlayer")
+	node.stop()
+	get_parent().change_scene("res://maps/tutorials/World2.tscn")
+
 func _on_BackButton_pressed():
 	get_parent().change_scene("res://gui/menu/Menu.tscn")
