f9c54e5abeef7a9de22e06506108380dec217a62
Emanuel <emanuel10.01@gmail.com>
2022-12-04 15:08:26 -0300

    IronRock/CopperOre

--- map_objects/IronRock.gd
+++ map_objects/IronRock.gd
@@ -1,16 +1,11 @@
 extends Sprite
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var IRON_SCENE = preload("res://items/IronOre.tscn")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func interact(held_item):
+	if held_item == null:
+		return IRON_SCENE.instance()
+	else:
+		return held_item
