4439f9ff6c1790ddb8cbe2f3824a17694579f0f7
Artur Souto <artursm007.2@gmail.com>
2022-12-04 11:45:57 -0300

    Rename Scenes and Adjust Scene
    Rename World2 to Level01
    Rename CopperOre to CopperRock
    Rename IronOre to IronRock
    Adjust Level01 Scene

--- gui/level_selector/LevelSelector.gd
+++ gui/level_selector/LevelSelector.gd
@@ -10,7 +10,7 @@ func _on_Level1Button_pressed():
 func _on_Level2Button_pressed():
 	var node = get_tree().get_root().get_node("Main/AudioStreamPlayer")
 	node.stop()
-	get_parent().change_scene("res://maps/tutorials/World2.tscn")
+	get_parent().change_scene("res://maps/levels/Level01.tscn")
 
 func _on_BackButton_pressed():
 	get_parent().change_scene("res://gui/menu/Menu.tscn")
